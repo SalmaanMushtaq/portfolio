39840935dc0d9533bcf9d96fea142a52312b7af5
Salmaan Mushtaq <khandaysalmaan00@gmail.com>
2024-03-15 22:44:48 +0530

    about me description changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -69,16 +69,20 @@ const About = () => {
                   textAlign: "justify",
                 }}
               >
-                &emsp;Hello there! 👋 I&apos;m SALMAAN MUSHTAQ, a passionate web
-                developer with a keen eye for design and a love for crafting
-                digital experiences. Based in Kashmir -India, I thrive on
+                &emsp; Hello there! 👋 I&apos;m SALMAAN MUSHTAQ, a passionate
+                web developer based in Kashmir, India. With a keen eye for
+                design and a love for crafting digital experiences, I thrive on
                 turning innovative ideas into reality through the power of code.
                 <br />
                 &emsp;🚀 My journey in web development began with a curiosity
                 about how the web works, and it has since evolved into a dynamic
-                skill set that spans HTML5, CSS3, and JavaScript. I believe in
-                the transformative potential of technology to solve real-world
-                problems and enhance user experiences.
+                skill set that spans HTML5, CSS3, JavaScript, ReactJs, JQuery,
+                Bootstrap, and Tailwindcss.
+                <br />
+                &emsp;Ready to see what I can do? Explore my portfolio to
+                witness the projects I&apos;ve brought to life, or feel free to
+                reach out to discuss potential collaborations or opportunities.
+                Let&apos;s create something amazing together!
               </p>
               <Box style={{ marginTop: "6%" }}>
                 <small
